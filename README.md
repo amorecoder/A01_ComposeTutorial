892ea9537f8643836df5c5df3e6405464a7fecd8
batspike <samlau.sam@gmail.com>
2022-06-26 20:56:58 +0800

    Lesson 3 - Material Design with MaterialTheme

--- README.md
+++ README.md
@@ -10,13 +10,14 @@ At the end of this journey, I hope to be an expert in designing beautiful and fu
 Let's begin !
 
 Important Notes and Observations:
-Lesson 1
+Lesson 1 - Composable functions
 1. Aside from using setContent from Activity or Fragment, a Composable can only be called from another Composable.
 2. The @Preview annotation can only be applied to a Composable that DO NOT take in any parameter.
 
-Lesson 2
+Lesson 2 - Layouts
 1. Use Column to arrange item vertically. Use Row to arrange items horizontally, and use Box to stack elements.
 2. Compose uses modifiers to change the composable's size, layout, appearance or add high-level interactions, such as making an element clickable. You can chain them to create richer composables.
 
-Lesson 3
+Lesson 3 - Material Design
+1. Material Design is built around three pillars: Color, Typography, and Shape.
 
--- app/src/main/java/com/example/a01_composetutorial/MainActivity.kt
+++ app/src/main/java/com/example/a01_composetutorial/MainActivity.kt
@@ -1,11 +1,15 @@
 package com.example.a01_composetutorial
 
+import android.content.res.Configuration
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
@@ -14,12 +18,20 @@ import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.example.a01_composetutorial.ui.theme.A01_ComposeTutorialTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            MessageCard(Message("Colleague", "Hey take a look at this."))
+            A01_ComposeTutorialTheme {
+                MessageCard(
+                    Message(
+                        "Colleague",
+                        "Take a look at Jetpack Compose! It simplifies UI development on Android."
+                    )
+                )
+            }
         }
     }
 }
@@ -28,28 +40,51 @@ data class Message(val author: String, val body: String)
 
 @Composable
 fun MessageCard(msg: Message) {
-    // Use Column to arrange item vertically. Use Row to arrange items horizontally, and
-    // use Box to stack elements.
     Row(modifier = Modifier.padding(8.dp)) {
         Image(
             painterResource(id = R.drawable.profile_pic),
             contentDescription = "Some profile picture",
             modifier = Modifier
-                .size(40.dp)
-                .clip(CircleShape),
+                .size(50.dp)
+                .clip(CircleShape)
+                .border(1.5.dp, MaterialTheme.colors.secondary, CircleShape),
             contentScale = ContentScale.Crop
         )
         Spacer(modifier = Modifier.width(8.dp))
         Column {
-            Text(text = msg.author)
+            Text(
+                text = msg.author,
+                color = MaterialTheme.colors.secondaryVariant,
+                style = MaterialTheme.typography.subtitle1
+            )
+            
             Spacer(modifier = Modifier.height(4.dp))
-            Text(text = msg.body)
+            
+            Surface(shape = MaterialTheme.shapes.medium, elevation = 4.dp) {
+                Text(
+                    text = msg.body,
+                    modifier = Modifier.padding(4.dp),
+                    style = MaterialTheme.typography.body2
+                )
+            }
         }
     }
 }
 
-@Preview
+@Preview(name = "Light Mode", showBackground = true)
+@Preview(
+    name = "Dark Mode",
+    uiMode = Configuration.UI_MODE_NIGHT_YES,
+    showBackground = true
+)
 @Composable
 fun PreviewMessageCard() {
-    MessageCard(msg = Message("Colleague", "Hey take a look at this."))
+    A01_ComposeTutorialTheme {
+        MessageCard(
+            Message(
+                "Colleague",
+                "Take a look at Jetpack Compose! It simplifies UI development on Android."
+            )
+        )
+    }
 }
